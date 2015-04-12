380b3ca5b3f2bb34e393ec01da858056562c239f
Chang Zong <changPC@c192034.laplan.net.ed.ac.uk>
2015-04-12 17:50:10 +0100

    Added sub pages for each of those subjects with displaying messages.

--- Resources/app.js
+++ Resources/app.js
@@ -6,6 +6,7 @@ var tabGroup = Titanium.UI.createTabGroup();
 
 var GlobelV = require('labels');
 var SendMsg = require('send_msg');
+var SubHandler = require('sub_handler');
 //
 // create base UI tab and root window
 //
@@ -14,6 +15,7 @@ var win1 = Titanium.UI.createWindow({
     title:'GeoInfo',
     backgroundColor:'#fff'
 });
+
 var tab1 = Titanium.UI.createTab({  
     icon:'images/KS_nav_views.png',
     title:'GeoInfo',
@@ -58,61 +60,39 @@ button.addEventListener('click', function(){
 	//Ti.API.info("Your current location is: ");
 	win1.remove(GlobelV.Labels.label1);
 	//Below is how you get return value from asynchronous function (like Titanium.Geolocation.getCurrentPosition)
-	//Ti.Geolocation.addEventListener('location', function(cb) { //You can disable this listener if you don't need it.
-		GlobelV.Labels.get_curr_pos(function(cb){
-			Titanium.API.info(cb);
-			var pos_printer = Titanium.UI.createLabel({
-				color:'#999',
-				text:'Your current location is: '+ cb.latitude + cb.longitude,
-				font:{fontSize:20,fontFamily:'Helvetica Neue'},
-				textAlign:'center',
-				width:'auto',
-				top: '5dp'
-			});
-			win1.add(pos_printer);
-			SendMsg.Functions.get_msg_with_pos(cb, function(ee){
-				//Ti.API.info(ee);
-				var list_block = '';
-				for (var i=0; i<ee.message.length; i++)
-				{
-					list_block = list_block + JSON.stringify(ee.subject[i]).replace(/(^"+|"+$)/g, '') + '\n\n';
-				}
-				Ti.API.info(list_block);
-				message_label = Ti.UI.createLabel({
-                	text : list_block,
-                	font : {
-                    	fontSize : '2dp',
-                    	fontWeight : 'bold'
-                	},
-                	height : 'auto',
-                	left : '10dp',
-                	top : '100dp',
-                	color : '#000',
-                	touchEnabled : false
-            	});
-            	win1.add(message_label);
-			});
-			
-		});
-	//});
-});
-
-/*button2 = Ti.UI.createButton({
-	width:150,
-	height:40,
-	top: 50,
-	title:"Send Message!"
-});
-
-win2.add(button2);
-
-button2.addEventListener('click', function(){
 	GlobelV.Labels.get_curr_pos(function(cb){
-		SendMsg.Functions.send_msg_with_pos(cb, function(ee){
-			Ti.API.info(ee);
+		Titanium.API.info(cb);
+		var pos_printer = Titanium.UI.createLabel({
+			color:'#999',
+			text:'Your current location is: '+ cb.latitude + cb.longitude,
+			font:{fontSize:20,fontFamily:'Helvetica Neue'},
+			textAlign:'center',
+			width:'auto',
+			top: '5dp'
 		});
+		win1.add(pos_printer);
+		SendMsg.Functions.get_msg_with_pos(cb, function(ee){
+			//Ti.API.info(ee);
+			var list_block = '';
+			for (var i=0; i<ee.message.length; i++)
+			{
+				var button_sub = Ti.UI.createButton({
+					width: 300,
+					height: 20,
+					top: 100 + 30*i,
+					title: JSON.stringify(ee.subject[i]).replace(/(^"+|"+$)/g, '')
+				});
+				win1.add(button_sub);
+				
+				button_sub.addEventListener('click', function(){
+					SubHandler.SubHandler.sub_msg_handler(this.title, ee);
+				});
+			}
+		});
+		
 	});
-});*/
+});
+
 
 var textField_id = SendMsg.Functions.input_text_adder()[0];
 var textField_msg = SendMsg.Functions.input_text_adder()[1];
---
+++ Resources/sub_handler.js
@@ -0,0 +1,47 @@
+/**
+ * @author Chang Zong
+ */
+exports.SubHandler = {
+	sub_msg_handler : function(title, content){
+		var btnBack = Ti.UI.createButton({
+			width: 40,
+			height: 20,
+			top: 50,
+			left: 10,
+			title: 'Back'
+		});
+		var win_sub = Ti.UI.createWindow({
+			backgroundColor: '#fff',
+			title: this.title,
+			navTintColor:'#fff',
+			leftNavButton: btnBack,
+			navBarHidden: false,
+			modal: true
+		});
+		var nav = Ti.UI.iOS.createNavigationWindow({
+			modal: true, 
+			window: win_sub
+		});
+		var sub_msg = content.message[content.subject.indexOf(title)];
+		//Ti.API.info(sub_msg);
+		var showing_msg = JSON.stringify(sub_msg).replace(/(^"+|"+$)/g, '').replace(/\\r\\n+/g, '\n');
+		var message_label = Ti.UI.createLabel({
+			text : showing_msg,
+			font : {
+		    	fontSize : '2dp',
+		    	fontWeight : 'bold'
+			},
+			height : 'auto',
+			left : '10dp',
+			top : '100dp',
+			color : '#000',
+			touchEnabled : false
+		});
+		win_sub.add(message_label);
+		win_sub.add(btnBack);
+		btnBack.addEventListener('click',function(e){
+			nav.closeWindow(win_sub, {animated:true});
+		});
+		win_sub.open({animated: true});
+	}
+};
